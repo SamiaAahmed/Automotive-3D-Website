78f26e02cb04a13da80ac7705e35912bb1c0315a
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-24 12:33:01 +0200

    preloader

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementById("text1").innerHTML = "EXPLORE NOW";
+
+// Pre Loader Script 
+window.addEventListener('DOMContentLoaded', function() {
+    const preloader = document.querySelector('.preloader');
+    if (preloader) {
+        setTimeout(function() {
+            preloader.classList.add('hide');
+        }, 500);
+    }
+});
