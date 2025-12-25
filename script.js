c77333bab841e8a48f6691645591e70f0390f50d
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-25 05:04:01 +0200

    section2

--- script.js
+++ script.js
@@ -17,3 +17,6 @@ document.getElementById("text5").innerHTML = "Premium helmets built for confiden
 document.getElementById("text6").innerHTML = "McLaren Helmet";
 document.getElementById("text7").innerHTML = "with visor plain";
 document.getElementById("text8").innerHTML = "View more";
+document.getElementById("text9").innerHTML = "Engine";
+document.getElementById("text10").innerHTML = "Breakdown";
+document.getElementById("text11").innerHTML = "A detailed look at the components that drive performance.";
